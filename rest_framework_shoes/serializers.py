21f4f692ab3151475eb72017a1965f1ea3804d71
Unknown <keybladeschoosen1@gmail.com>
2020-02-20 11:49:59 -0500

    Added Serializers

--- rest_framework_shoes/serializers.py
+++ rest_framework_shoes/serializers.py
@@ -1,1 +1,35 @@
-from rest_framework.s
+from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework_shoes import models
+
+
+class ManufacturerSerializers(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Manufacturer
+        fields = ['id', 'name', 'website']
+
+
+class ShoeTypeSerializers(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.ShoeType
+        fields = ['id', 'style']
+
+
+class ShoeColorSerializers(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.ShoeColor
+        fields = ['id', 'color']
+
+
+class ShoeSerializers(HyperlinkedModelSerializer):
+    class Meta:
+        model = models.Shoe
+        fields = [
+            'id',
+            'size',
+            'brand',
+            'manufacturer',
+            'color',
+            'material',
+            'shoe_type',
+            'fasten_type'
+        ]
